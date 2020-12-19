8098385efa7485251c9f3cf55914a05d2c687d63
Ivan Pli <plisyakov.ivan@mail.ru>
2020-12-19 16:48:03 +0700

    12.0.3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routerCards);
 app.use('/', routerUsers)
 
