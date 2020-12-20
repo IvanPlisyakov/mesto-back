f41f74fc2420d488a61aa790141f981ea9dddb20
Ivan Pli <plisyakov.ivan@mail.ru>
2020-12-20 21:11:55 +0700

    12.1.1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routerCards = require('./routes/cards.js');
 const routerUsers = require('./routes/users.js');
 const path = require('path');
@@ -6,6 +7,12 @@ const path = require('path');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routerCards);
